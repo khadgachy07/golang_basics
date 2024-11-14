c27ec6f68a0ce5e82c52424b11d3e84dc5263512
“khadgachy07” <khadgachy07@gmail.com>
2024-11-14 22:06:25 +0545

    variable complete

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const LoginToken = "hafhakhfaljflafljak" //public
+
 func main() {
 	var username string = "khadgachy07"
 	fmt.Println(username)
@@ -14,4 +16,31 @@ func main() {
 	var smallVal uint8 = 255
 	fmt.Println(smallVal)
 	fmt.Printf("Type of variable is %T \n", smallVal)
+
+	var smallFloat float64 = 255.56565665
+	fmt.Println(smallFloat)
+	fmt.Printf("Type of variable is %T \n", smallFloat)
+
+	// default values and alias
+	var defaultVal int
+	fmt.Println(defaultVal)
+	fmt.Printf("Type of variable is %T \n", defaultVal)
+
+	var defaultString string
+	fmt.Println(defaultString)
+	fmt.Printf("Type of variable is %T \n", defaultString)
+
+	//implict type
+	var website = "localhost:3000"
+	fmt.Println(website)
+	fmt.Printf("Type of variable is %T \n", website)
+
+	// no var style
+	numberOfUser := 6666
+	fmt.Println(numberOfUser)
+	fmt.Printf("Type of variable is %T \n", numberOfUser)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Type of variable is %T \n", LoginToken)
+
 }
