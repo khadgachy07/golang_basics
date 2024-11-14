c4587c5911d7a0c139e8f3abd6eeb520e7bb934f
“khadgachy07” <khadgachy07@gmail.com>
2024-11-14 17:21:20 +0545

    variable bool n uint8

--- 02variables/main.go
+++ 02variables/main.go
@@ -6,4 +6,12 @@ func main() {
 	var username string = "khadgachy07"
 	fmt.Println(username)
 	fmt.Printf("Type of variable is %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Type of variable is %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Type of variable is %T \n", smallVal)
 }
