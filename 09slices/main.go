bd64e43d81e0640bced7b16f71adc616f54c7f64
“khadgachy07” <khadgachy07@gmail.com>
2024-11-15 23:24:25 +0545

    Slices v2

--- 09slices/main.go
+++ 09slices/main.go
@@ -8,14 +8,14 @@ import (
 func main() {
 	fmt.Println("Welcome to Slices in Golang")
 
-	var fruitList = []string{"Apple", "Tomato", "Peach"}
-	fmt.Println("FruitList is ", fruitList)
+	// var fruitList = []string{"Apple", "Tomato", "Peach"}
+	// fmt.Println("FruitList is ", fruitList)
 
-	fruitList = append(fruitList, "Mango", "Banana")
-	fmt.Println("New FruitList is ", fruitList)
+	// fruitList = append(fruitList, "Mango", "Banana")
+	// fmt.Println("New FruitList is ", fruitList)
 
-	fruitList = append(fruitList[1:3]) // starts with index 1 to index 3....ps: won't include index 3
-	fmt.Println("New FruitList is ", fruitList)
+	// fruitList = append(fruitList[1:3]) // starts with index 1 to index 3....ps: won't include index 3
+	// fmt.Println("New FruitList is ", fruitList)
 
 	highscore := make([]int, 4)
 
@@ -29,7 +29,17 @@ func main() {
 	highscore = append(highscore, 333, 555)
 	fmt.Println(highscore)
 
-	// sort.Ints(highscore)
+	sort.Ints(highscore)
 	fmt.Println(highscore)
 	fmt.Println(sort.IntsAreSorted(highscore))
+
+	// how to remove a value from slices based on index
+
+	var courses = []string{"reactjs", "javascript", "swift", "python", "rust"}
+	fmt.Println(courses)
+
+	var index int = 2
+	courses = append(courses[:index], courses[index+1:]...)
+	fmt.Println(courses)
+
 }
