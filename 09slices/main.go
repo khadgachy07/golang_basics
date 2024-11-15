ced25f61f4137a95dfbc5690cc1e45a372129c09
“khadgachy07” <khadgachy07@gmail.com>
2024-11-15 22:45:46 +0545

    Slices in Golang

---
+++ 09slices/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Welcome to Slices in Golang")
+
+	var fruitList = []string{"Apple", "Tomato", "Peach"}
+	fmt.Println("FruitList is ", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+	fmt.Println("New FruitList is ", fruitList)
+
+	fruitList = append(fruitList[1:3]) // starts with index 1 to index 3....ps: won't include index 3
+	fmt.Println("New FruitList is ", fruitList)
+
+	highscore := make([]int, 4)
+
+	highscore[0] = 111
+	highscore[1] = 666
+	highscore[2] = 444
+	highscore[3] = 333
+
+	fmt.Println(highscore)
+
+	highscore = append(highscore, 333, 555)
+	fmt.Println(highscore)
+
+	// sort.Ints(highscore)
+	fmt.Println(highscore)
+	fmt.Println(sort.IntsAreSorted(highscore))
+}
